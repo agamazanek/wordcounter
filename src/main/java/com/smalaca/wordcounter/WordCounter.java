305fcbaccc9f7c7a1eb20c5ae17e67ca90810921
smalaca <ab13.krakow@gmail.com>
2018-01-25 00:26:00 +0100

    Should count words occurences in the given sentence

--- src/main/java/com/smalaca/wordcounter/WordCounter.java
+++ src/main/java/com/smalaca/wordcounter/WordCounter.java
@@ -1,13 +1,17 @@
 package com.smalaca.wordcounter;
 
+import static java.util.Arrays.asList;
+
 class WordCounter {
+    private static final String SEPARATOR = " ";
+
     private final String sentence;
 
     WordCounter(String sentence) {
         this.sentence = sentence;
     }
 
-    int count(String word) {
-        return sentence.contains(word) ? 1 : 0;
+    long count(String word) {
+        return asList(sentence.split(SEPARATOR)).stream().filter(word::equals).count();
     }
 }
--- src/test/java/com/smalaca/wordcounter/WordCounterTest.java
+++ src/test/java/com/smalaca/wordcounter/WordCounterTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class WordCounterTest {
-    private static final String SOME_SENTENCE = "Hello! Hello everyone";
+    private static final String SOME_SENTENCE = "Hello Hello everyone";
     private static final String SOME_WORD = "some";
     private static final String EVERYONE_WORD = "everyone";
+    private static final String HELLO_WORD = "Hello";
     private static final int ZERO = 0;
     private static final int ONE = 1;
+    private static final int TWO = 2;
 
     @Test
     public void shouldRecognizeWhenWordIsNotPresentInSentence() {
@@ -23,5 +25,12 @@ public class WordCounterTest {
         WordCounter wordCounter = new WordCounter(SOME_SENTENCE);
 
         assertEquals(ONE, wordCounter.count(EVERYONE_WORD));
+    }
+
+    @Test
+    public void shouldCountWordOccurencesInSentence() {
+        WordCounter wordCounter = new WordCounter(SOME_SENTENCE);
+
+        assertEquals(TWO, wordCounter.count(HELLO_WORD));
     }
 }
